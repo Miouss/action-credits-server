3280ebe3fa03a4b5a2d394089e3c0c8cd0ea7b43
Miouss <samir.ghabi@gmail.com>
2023-12-06 19:12:33 +0100

    setup basic functionnality

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
 import express from "express";
+import cors from "cors";
+import { Request, Response, NextFunction } from "express";
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(cors());
+
 app.get("/", (_, res) => {
   res.send("Hello World!");
+});
+
+app.post("/api/consume", (req, res) => {
+  const { credits } = req.body;
+  if (credits <= 0) {
+    throw new Error("No credits left");
+  } else {
+    res.status(200).send("Credits consumed");
+  }
+});
+
+app.use((err: any, _: Request, res: Response, __: NextFunction) => {
+  console.error(err.stack);
+  res.status(400).send(err.message);
 });
 
 app.listen(port, () => {
