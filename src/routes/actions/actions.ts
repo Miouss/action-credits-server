4fb455d93ab398ef1646f3951ac813b2f8771178
Miouss <samir.ghabi@gmail.com>
2023-12-07 14:45:17 +0100

    Add simple API endpoint to get credits / actions

--- src/routes/actions/actions.ts
+++ src/routes/actions/actions.ts
@@ -2,4 +2,15 @@ import { Router } from 'express';
 
 const actions = Router();
 
+enum Action {
+    INVITE = 'Invite',
+    SEND_MESSAGE = 'Send Message',
+    VISIT = 'Visit',
+}
+
+actions.get('/', (_, res) => {
+    const actions = Object.values(Action);
+    res.json(actions);
+});
+
 export { actions };
