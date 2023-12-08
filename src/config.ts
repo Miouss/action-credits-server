43db1f6a6e43809d4ee6a57acb7f525b942d3e3e
Miouss <samir.ghabi@gmail.com>
2023-12-08 18:29:49 +0100

    fix randomizer of credits

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,9 @@ import { User, ActionName } from "./enums";
 import { UserActions } from "./types";
 
 export const USERS_ACTIONS_FILE_PATH = "./src/data/users-actions.json";
-export const MAX_CREDITS = 5;
-export const MAX_CREDITS_PERCENT = 100;
-export const MIN_CREDITS_PERCENT = 80;
+export const CREDITS = 100;
+export const MAX_CREDITS_PERCENT = 50;
+export const MIN_CREDITS_PERCENT = 50;
 export const DEFAULT_USERS: User[] = Object.values(User);
 export const DEFAULT_ACTIONS: ActionName[] = Object.values(ActionName);
 
@@ -14,7 +14,7 @@ export const DEFAULT_USERS_ACTIONS: UserActions[] = DEFAULT_USERS.map(
     actions: DEFAULT_ACTIONS.map((name) => ({
       name,
       credits: randomMinMaxPercent(
-        MAX_CREDITS,
+        CREDITS,
         MIN_CREDITS_PERCENT,
         MAX_CREDITS_PERCENT
       ),
@@ -23,11 +23,12 @@ export const DEFAULT_USERS_ACTIONS: UserActions[] = DEFAULT_USERS.map(
 );
 
 function randomMinMaxPercent(
-  max: number,
+  value: number,
   minPercent: number,
   maxPercent: number
 ) {
-  const min = Math.ceil(max * (maxPercent / 100) * (minPercent / 100));
+  const max = Math.floor(value * (maxPercent / 100));
+  const min = Math.ceil(max * (minPercent / 100));
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
