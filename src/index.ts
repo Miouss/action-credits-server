668a895493e2b70f855975128d5516c18391baee
Miouss <samir.ghabi@gmail.com>
2023-12-09 01:05:15 +0100

    init server into a separate file

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,5 @@
-import express from "express";
-import cors from "cors";
-import { Request, Response, NextFunction } from "express";
-import { actions, auth } from "./routes";
-
 import dotenv from "dotenv";
-import { setupUsersActionsFile } from "./data/utils";
+import { initServer } from "./server";
 dotenv.config();
 
-const app = express();
-const port = 3001;
-
-app.use(express.json());
-app.use(cors());
-
-app.use("/api/actions", actions);
-app.use("/api/auth", auth);
-
-app.use((err: any, _: Request, res: Response, __: NextFunction) => {
-  console.error(err.message);
-  res.status(400).send(err.message);
-});
-
-app.listen(port, () => {
-  setupUsersActionsFile();
-  console.log(`listening on port ${port}`);
-});
+initServer();
---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+
+import { actions, auth } from "./routes";
+import { setupUsersActionsFile } from "./data/utils";
+
+export function initServer() {
+  const app = express();
+  const port = 3001;
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use("/api/actions", actions);
+  app.use("/api/auth", auth);
+
+  app.use((err: any, _: Request, res: Response, __: NextFunction) => {
+    console.error(err.message);
+    res.status(400).send(err.message);
+  });
+
+  app.listen(port, () => {
+    setupUsersActionsFile();
+    console.log(`listening on port ${port}`);
+  });
+}
