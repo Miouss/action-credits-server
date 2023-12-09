4a35c1f7aa28d0309ae844c3062dfe2a2b5caa8b
Miouss <samir.ghabi@gmail.com>
2023-12-09 12:59:10 +0100

    fix response

--- src/routes/actions/middlewares/sendConfirmation.ts
+++ src/routes/actions/middlewares/sendConfirmation.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
 
 export function sendConfirmation(_: Request, res: Response, __: NextFunction) {
-  res.status(200);
+  res.sendStatus(200);
 }
