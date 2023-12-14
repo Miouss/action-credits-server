25492f5386fcd4a09d1fddb8f3f2ab329fa00f60
Miouss <samir.ghabi@gmail.com>
2023-12-14 18:10:47 +0100

    set proper config

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,8 @@ import { DataProviderType } from "./data";
 
 export const DATA_PROVIDER_TYPE: DataProviderType = "file";
 
-export const REFRESH_CREDITS_INTERVAL = 111130000; // ms
+const ONE_HOUR = 1000 * 60 * 60;
 
-export const EXECUTION_INTERVAL = 5000; // ms
+export const REFRESH_CREDITS_INTERVAL = ONE_HOUR * 24; // ms
+
+export const EXECUTION_INTERVAL = ONE_HOUR * 2; // ms
