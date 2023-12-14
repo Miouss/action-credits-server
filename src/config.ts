41c2c7e7f614a0d87146f712ca714663d341f27f
Miouss <samir.ghabi@gmail.com>
2023-12-14 18:49:07 +0100

    easier readable config file

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,9 @@ import { DataProviderType } from "./data";
 
 export const DATA_PROVIDER_TYPE: DataProviderType = "file";
 
-const ONE_MINUTE = 1000 * 60;
+const ONE_SECOND = 1000;
+
+const ONE_MINUTE = ONE_SECOND * 60;
 
 const ONE_HOUR = ONE_MINUTE * 60;
 
