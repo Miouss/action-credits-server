ec8283dcb73a7a11c8de65b4077c2df0edd4f40a
Miouss <samir.ghabi@gmail.com>
2023-12-14 11:04:58 +0100

    fix data > actions test

--- src/data/file-based/actions.test.ts
+++ src/data/file-based/actions.test.ts
@@ -1,39 +1,33 @@
 import jsonfile from "jsonfile";
 import { ACTIONS_FILE_PATH, getActions, updateActions } from "./actions";
 import { ActionName } from "../../types/enums";
-import { Queue } from "../../types/types";
+import { Actions } from "../../types/types";
 
 jest.mock("jsonfile");
 
 describe("actions", () => {
-  const queue: Queue = {
-    pending: [ActionName.SEND_MESSAGE],
-    executed: [ActionName.INVITE],
+  const actions: Actions = {
+    items: [
+      { name: ActionName.INVITE, credits: 10 },
+      { name: ActionName.SEND_MESSAGE, credits: 2 },
+    ],
+    id: "123",
   };
 
-  (jsonfile.readFile as jest.Mock).mockReturnValue(queue);
+  (jsonfile.readFile as jest.Mock).mockReturnValue(actions);
 
   describe("getActions", () => {
-    it(`should call jsonfile.readFile with path ${ACTIONS_FILE_PATH} and return the queue`, async () => {
+    it(`should call jsonfile.readFile with path ${ACTIONS_FILE_PATH} and return the actions`, async () => {
       // Act
       const result = await getActions();
 
       // Assert
       expect(jsonfile.readFile).toHaveBeenCalledWith(ACTIONS_FILE_PATH);
-      expect(result).toBe(queue);
+      expect(result).toBe(actions);
     });
   });
   describe("updateActions", () => {
     it(`should call jsonfile.writeFile with path ${ACTIONS_FILE_PATH}`, async () => {
-      // Arrange
-      const actions = {
-        items: [
-          { name: ActionName.INVITE, credits: 10 },
-          { name: ActionName.SEND_MESSAGE, credits: 2 },
-        ],
-        id: "123",
-      };
-
       // Act
       await updateActions(actions);
 
